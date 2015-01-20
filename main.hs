88043235e9a427e383ef6fd6f89e44ca964716e0
Brett Hall <bhall@wyatt.com>
2015-01-20 14:33:56 -0800

    Swtiched to using lenses

--- main.hs
+++ main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE  TemplateHaskell, Rank2Types#-}
+
 module Main where
 
 import Harriss
@@ -5,51 +7,33 @@ import Graphics.UI.Gtk
 import Graphics.Rendering.Cairo
 import Graphics.UI.Gtk.Gdk.Events
 import Data.IORef
+import Control.Lens
 
-main = do
-  initGUI
-  win <- windowNew
-  onDestroy win mainQuit
-  windowSetDefaultSize win 300 300
-
-  vBox <- vBoxNew False 0
-  containerAdd win vBox
-
-  guiState <- newIORef $ GUIState 0 True False False False
-
-  plot <- drawingAreaNew
-  boxPackStart vBox plot PackGrow 0
-  onExpose plot $ drawPlot plot guiState
-
-  --need to force repaint in signal handlers
-  gen <- spinButtonNewWithRange 0.0 100000.0 1.0
-  boxPackStart vBox gen PackNatural 1
-  onValueSpinned gen (updateGeneration gen guiState)
-  
-  -- should probably be using lenses below
-  drawCurves <- checkButtonNewWithMnemonic "Draw _Curves"
-  boxPackStart vBox drawCurves PackNatural 1
-  toggleButtonSetActive drawCurves True
-  on drawCurves buttonActivated (updateCheckedState drawCurves guiState (\c s -> s {drawCurves = c}))
-
-  drawSquares <- checkButtonNewWithMnemonic "Draw _Squares"
-  boxPackStart vBox drawSquares PackNatural 1
-  on drawSquares buttonActivated (updateCheckedState drawSquares guiState (\c s -> s {drawSquares = c}))
- 
-  colorSquares <- checkButtonNewWithMnemonic "Color _Squares"
-  boxPackStart vBox colorSquares PackNatural 1
-  on colorSquares buttonActivated (updateCheckedState colorSquares guiState (\c s -> s {colorSquares = c}))
-
-  drawSplitRects <- checkButtonNewWithMnemonic "Draw _Split Rectangles"
-  boxPackStart vBox drawSplitRects PackNatural 1
-  on drawSplitRects buttonActivated (updateCheckedState drawSplitRects guiState (\c s -> s {drawSplitRects = c}))
-
-  widgetShowAll win
-  
-  mainGUI
-
-data GUIState = GUIState {numGens::Int, drawCurves::Bool, drawSquares::Bool, colorSquares::Bool, drawSplitRects::Bool}
+data GUIState = GUIState {_numGens::Int, _drawCurves::Bool, _drawSquares::Bool, _colorSquares::Bool, _drawSplitRects::Bool}
               deriving Show
+
+$(makeLenses ''GUIState)
+
+makeCheckButton :: String -> Lens' GUIState Bool -> IORef GUIState -> VBox -> IO CheckButton
+makeCheckButton text lens stateRef box = do
+  button <- checkButtonNewWithMnemonic text
+  boxPackStart box button PackNatural 1
+  state <- readIORef stateRef
+  toggleButtonSetActive button $ state^.lens
+  on button buttonActivated (updateCheckedState button stateRef lens)
+  return button
+    where
+      updateCheckedState :: CheckButton -> IORef GUIState -> Lens' GUIState Bool -> IO ()
+      updateCheckedState button state lens = do
+                                           checked <- toggleButtonGetActive button 
+                                           modifyIORef state $ lens.~checked
+                                           readIORef state >>= print
+                                                                   
+updateGeneration :: SpinButton -> IORef GUIState -> IO ()
+updateGeneration sb state = do
+  g  <- spinButtonGetValue sb
+  modifyIORef state $ numGens.~(round g)
+  readIORef state >>= print
 
 drawPlot :: DrawingArea -> IORef GUIState -> Event -> IO Bool
 drawPlot plot guiState _ = do
@@ -82,15 +66,32 @@ drawPlot plot guiState _ = do
 
   return True
 
-updateGeneration :: SpinButton -> IORef GUIState -> IO ()
-updateGeneration sb state = do
-  g  <- spinButtonGetValue sb
-  print g
-  modifyIORef state (\s -> s {numGens = round g})
-  readIORef state >>= print
-  
 
-updateCheckedState :: CheckButton -> IORef GUIState -> (Bool -> GUIState -> GUIState) -> IO ()
-updateCheckedState button state update = do
-    checked <- toggleButtonGetActive button 
-    modifyIORef state (update checked)
+main = do
+  initGUI
+  win <- windowNew
+  onDestroy win mainQuit
+  windowSetDefaultSize win 300 300
+
+  vBox <- vBoxNew False 0
+  containerAdd win vBox
+
+  guiState <- newIORef $ GUIState 0 True False False False
+
+  plot <- drawingAreaNew
+  boxPackStart vBox plot PackGrow 0
+  onExpose plot $ drawPlot plot guiState
+
+  --need to force repaint in signal handlers
+  gen <- spinButtonNewWithRange 0.0 100000.0 1.0
+  boxPackStart vBox gen PackNatural 1
+  onValueSpinned gen (updateGeneration gen guiState)
+  
+  makeCheckButton "Draw _Curves" drawCurves guiState vBox
+  makeCheckButton "Draw _Squares" drawSquares guiState vBox
+  makeCheckButton "Color _Squares" colorSquares guiState vBox
+  makeCheckButton "Draw _Split Rectangles" drawSplitRects guiState vBox
+
+  widgetShowAll win
+  
+  mainGUI
