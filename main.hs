a8dcadc4808a939f74c6b4fea49a35282e673690
Brett Hall <bhall@wyatt.com>
2015-01-23 10:30:29 -0800

    Added GUI for curve drawing parameters

--- main.hs
+++ main.hs
@@ -16,15 +16,15 @@ data GUIState = GUIState {_numGens::Int,
                           _colorSquares::Bool, 
                           _drawSplitRects::Bool,
                           _lineThickness::Double,
-                          _entryAngle::Angle,
+                          _entryAngleDegrees::Angle,
                           _magMult::MagnitudeMult}
               deriving Show
 
 $(makeLenses ''GUIState)
 
 debugPrintM :: (Monad m, Show a) => a -> m ()
---debugPrintM a = traceShow a $ return ()
-debugPrintM _ = return ()
+debugPrintM a = traceShow a $ return ()
+--debugPrintM _ = return ()
 
 makeCheckButton :: String -> Lens' GUIState Bool -> IORef GUIState -> VBox -> DrawingArea -> IO CheckButton
 makeCheckButton text lens stateRef box plot = do
@@ -49,17 +49,92 @@ updateGeneration sb state plot = do
   readIORef state >>= debugPrintM
   widgetQueueDraw plot
 
+parseDouble :: String -> Maybe Double
+parseDouble s = parse $ reader s
+    where
+      reader = (readsPrec 0)::(ReadS Double)
+      parse [(n, leftover)] | null leftover = Just n
+                            | otherwise = Nothing
+      parse _ = if (head s) == '.'
+                then parse $ reader $ '0':s
+                else Nothing
+               
 makeNumEntry :: String -> Lens' GUIState Double -> IORef GUIState -> VBox -> DrawingArea -> IO Entry
 makeNumEntry text lens stateRef vBox plot = do  
-  hBox <- hBoxNew
+  hBox <- hBoxNew False 1
   boxPackStart vBox hBox PackNatural 0
-  label <- labelNew text
+  label <- labelNew $ Just text
   boxPackStart hBox label PackNatural 0
   entry <- entryNew
   boxPackStart hBox entry PackNatural 0
   state <- readIORef stateRef 
-  entrySetText entry $ show (state^.lens)
-  --How to handle validation???
+  entrySetText entry $ show $ state^.lens
+  needRedrawRef <- newIORef False
+  idInsertRef <- newIORef undefined
+  id <- entry `on` insertText $ \str pos -> do
+          debugPrintM "Adding text"
+          curText <- entryGetText entry
+          debugPrintM $ "old string = " ++ curText
+          let newText = (take pos curText) ++ str ++ (drop pos curText)
+          debugPrintM $ "new string = " ++ newText
+          case parseDouble newText of
+            Just n -> do
+              debugPrintM $ "new value = " ++ show n
+              modifyIORef stateRef $ lens.~n
+              modifyIORef needRedrawRef $ const True
+              id <- readIORef idInsertRef
+              signalBlock id
+              pos' <- editableInsertText entry str pos
+              signalUnblock id
+              stopInsertText id
+              return pos'
+            Nothing -> do
+              debugPrintM "bad value"
+              id <- readIORef idInsertRef
+              signalBlock id
+              pos' <- editableInsertText entry "" pos
+              signalUnblock id
+              stopInsertText id
+              return pos'
+  writeIORef idInsertRef id
+  idDeleteRef <- newIORef undefined
+  dd <- entry `on` deleteText $ \start end -> do
+          debugPrintM "Deleting text"
+          curText <- entryGetText entry
+          debugPrintM $ "old string = " ++ curText
+          let newText = (take start curText) ++ (drop end curText)
+          debugPrintM $ "new string = " ++ newText
+          case parseDouble newText of
+            Just n -> do
+              debugPrintM $ "new value = " ++ show n
+              modifyIORef stateRef $ lens.~n
+              modifyIORef needRedrawRef $ const True
+              id <- readIORef idDeleteRef
+              signalBlock id
+              pos' <- editableDeleteText entry start end
+              signalUnblock id
+              stopDeleteText id
+            Nothing -> do
+              debugPrintM "bad value"
+              id <- readIORef idDeleteRef
+              signalBlock id
+              pos' <- editableDeleteText entry start start
+              signalUnblock id
+              stopDeleteText id
+  writeIORef idDeleteRef dd
+  entry `on` focusOutEvent $ do
+    needRedraw <- liftIO $ readIORef needRedrawRef
+    if needRedraw
+       then (liftIO $ widgetQueueDraw plot) >> (liftIO $ modifyIORef needRedrawRef (const False))
+       else return ()
+    return False
+  entry `on` entryActivate $ do
+    debugPrintM "activate" 
+    needRedraw <- readIORef needRedrawRef
+    if needRedraw
+       then widgetQueueDraw plot >> modifyIORef needRedrawRef (const False)
+       else return ()
+  return entry
 
 plotScale :: Double
 plotScale = 10000.0
@@ -67,10 +142,10 @@ plotScale = 10000.0
 data RectType = First | Second deriving (Show, Eq)
                       
 angle = 45.0*pi/180.0
-magMult = 0.4
+magMultFixed = 0.4
 
 getCurves :: Division -> (BCurve, BCurve)
-getCurves = curvesForDivision angle magMult
+getCurves = curvesForDivision angle magMultFixed
 
 drawPlot :: DrawingArea -> IORef GUIState -> Event -> IO Bool
 drawPlot plot guiState _ = do
@@ -158,23 +233,34 @@ main = do
   onDestroy win mainQuit
   windowSetDefaultSize win 800 500
 
-  vBox <- vBoxNew False 0
-  containerAdd win vBox
+  hBox <- hBoxNew False 0
+  containerAdd win hBox
 
-  guiState <- newIORef $ GUIState 0 True False False False
+  guiState <- newIORef $ GUIState 0 True False False False 10.0 45.0 0.4
 
   plot <- drawingAreaNew
-  boxPackStart vBox plot PackGrow 0
+  boxPackStart hBox plot PackGrow 0
   onExpose plot $ drawPlot plot guiState
 
+  vBox <- vBoxNew False 0
+  boxPackStart hBox vBox PackNatural 3
+
+  genBox <- hBoxNew False 0
+  boxPackStart vBox genBox PackNatural 0  
+  genLabel <- labelNew $ Just "Generation"
+  boxPackStart genBox genLabel PackNatural 0
   gen <- spinButtonNewWithRange 0.0 100000.0 1.0
-  boxPackStart vBox gen PackNatural 0
+  boxPackStart genBox gen PackNatural 0
   onValueSpinned gen (updateGeneration gen guiState plot)
   
   makeCheckButton "Draw _Curves" drawCurves guiState vBox plot
   makeCheckButton "Draw _Squares" drawSquares guiState vBox plot
   makeCheckButton "Color _Squares" colorSquares guiState vBox plot
   makeCheckButton "Draw _Split Rectangles" drawSplitRects guiState vBox plot
+
+  makeNumEntry "Line Width" lineThickness guiState vBox plot
+  makeNumEntry "Entry Angle (degrees)" entryAngleDegrees guiState vBox plot
+  makeNumEntry "Mag Multiplier" magMult guiState vBox plot
 
   widgetShowAll win
   widgetQueueDraw plot
